c24e319027b6d31679748cb6df6f6a7e0fcdf92a
Jochen <jowylick@microsoft.com>
2024-09-16 13:36:01 +0000

    #3

---
+++ src/lmgtfu-agent/hack.py
@@ -0,0 +1,16 @@
+import autogen
+
+config_list_gpt4 = autogen.config_list_from_json(
+    "OAI_CONFIG_LIST",
+    filter_dict={
+        "model": ["gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-v0314"],
+    },
+)
+
+gpt4_config = {
+    "cache_seed": 42,  # change the cache_seed for different trials
+    "temperature": 0,
+    "config_list": config_list_gpt4,
+    "timeout": 120,
+}
+
