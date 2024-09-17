bae36f305d0a3ea168da2bf41e214699c6c400c6
Dylan de Jong <dylan@gps-speedsurfing.com>
2024-09-17 15:36:56 +0200

    1

--- src/lmgtfu-agent/agents/arm_check.py
+++ src/lmgtfu-agent/agents/arm_check.py
@@ -3,7 +3,8 @@ config_list_gpt4 = autogen.config_list_from_json("OAI_CONFIG_LIST")
 
 arm_check_prompt = """
 You are an AI agent that validates if the requested Azure Service with it's configuration in implemented in the ARM API specification.
-Only use this URL for your reseach: Https:...
+The ARM API specification is a JSON file that describes the Azure services and their configuration options
+The ARM API specification cam be found here: https://learn.microsoft.com/en-us/azure/templates/
 """
 
 agent = autogen.AssistantAgent(
