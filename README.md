2eecf338cbf82f4f10a98bc692470ee1b96d7bee
Jochen <jowylick@microsoft.com>
2024-09-20 08:43:47 +0000

    Images and readme

--- README.md
+++ README.md
@@ -1,6 +1,44 @@
+# MSFT 2024 Hackathon repository: "LetMeGoogleThatAzureQuestionForYou-Agent"
+![Image](assets/agent_banner.png)
 
 
-## Get started
+[Hackathon project site](https://hackbox.microsoft.com/hackathons/hackathon2024/project/73988)
+
+
+## Introduction
+Us techies in the field (CSAs and TSPs) get hammered with technical questions from customers (and colleagues) that are often just a couple of Google (or Bing :) ) searches away. The polite thing to do is to do that Google search and politely respond with a summary of your findings and some links.
+
+__LetMeGoogleThatAzureQuestionForYou-Bot__ to the rescue! The idea is to build an AI agent that does this searching and finding of data for you and composes an answer for you to hand back.
+
+
+## AutoGen
+
+Our solution leverages [https://microsoft.github.io/autogen/](https://microsoft.github.io/autogen/). Which is explained by [Dr. Andrew Ng](https://www.andrewng.org/):
+
+> Today, we mostly use LLMs in zero-shot mode, prompting a model to generate final output token by token without revising its work. This is akin to asking someone to compose an essay from start to finish, typing straight through with no backspacing allowed, and expecting a high-quality result. Despite the difficulty, LLMs do amazingly well at this task!
+>
+> With an agentic workflow, however, we can ask the LLM to iterate over a document many times. For example, it might take a sequence of steps such as:
+> - Plan an outline.
+> - Decide what, if any, web searches are needed to gather more information.
+> - Write a first draft.
+> - Read over the first draft to spot unjustified arguments or extraneous information.
+> - Revise the draft taking into account any weaknesses spotted.
+> - And so on.
+> 
+> This iterative process is critical for most human writers to write good text. With AI, such an iterative workflow yields much better results than writing in a single pass.
+
+[Source](https://x.com/AndrewYNg/status/1770897666702233815?lang=en#:~:text=Today%2C%20we%20mostly,a%20single%20pass.)
+
+## Agents
+
+| Agent name       | Role    | Link   |   |
+|------------------|---------|----------|---|
+| Research manager | Creates the research plan, delegates the tasks and is only one to interact with 'us' | [./src/lmgtfu-agent/agents/research_manager.py] | ![](./assets/mgr.png) |
+| Researcher       | Does research. Can search the web, read webpages and summarize content     | asd      | ![](./assets/res.png) |
+| WAF       | Expert on Azure Well Architected. Checks relevant queries for the 5-pillars     | asd      | ![](./assets/waf.png) |
+| Critic           | Quality Assurance of the research and response. Checks whether the original question was properly answered. Whether evidence was provided, etc.     | asd      | ![](./assets/result_critic.png) |
+
+
 
 ### Pre-reqs
 
---
+++ src/lmgtfu-agent/skills/get_arm_spec.py
@@ -0,0 +1,88 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import AzureChatOpenAI
+from langchain.chains import LLMChain
+import json
+import os
+
+from skills.websearch import google_search
+from skills.scrape_website import scrape_website
+
+def go_get_arm_spec(serviceName: str) -> str:
+    """
+    Retrieves the Azure Resource Manager (ARM) specification URL for a given Azure service.
+    This function performs the following steps:
+    1. Conducts a Google search to find URLs related to the ARM templates on the Microsoft Learn site.
+    2. Uses an Azure OpenAI model to filter and identify the most relevant URL that contains the ARM template for the specified service.
+    3. Scrapes the identified URL to retrieve the ARM specification.
+    4. Summarizes the retrieved ARM specification text.
+    Args:
+        serviceName (str): The name of the Azure service for which the ARM specification is needed.
+    Returns:
+        str: A summarized text of the ARM specification for the specified service.
+    """
+    armSpecSite = "learn.microsoft.com/en-us/azure/templates"
+    searchResults = google_search(serviceName, armSpecSite)
+
+    print(f"Found {len(searchResults)} search results for {serviceName} on {armSpecSite}")
+
+    endpoint = os.getenv("OAI_ENDPOINT")
+    key = os.getenv("OAI_KEY")
+    api_version = os.getenv("OAI_VERSION")
+
+    llm = AzureChatOpenAI(temperature = 0, model = "gpt-4-32k", azure_endpoint=endpoint, api_key=key, api_version=api_version)
+    
+    # Selec the URL from the list of search results
+    tpl = """
+        In the list of URLs, find the best best candidate to contain information about {service_name}. 
+        - Return the URL that contains the Azure Resource Provider name of the Azure service name
+        - Otherwise, return the URL that contains the service name 
+        - Ensure to point to the URL that contains the ARM template for the service, by including URL Query Parameters "?pivots=deployment-language-arm-template"
+        - Respond with the URL and only the URL
+
+        ## begin list of URLs
+        {searchResults}
+        ## end list of URLs
+    """
+    filter_tpl = PromptTemplate(template=tpl, input_variables=["service_name", "searchResults"])   
+
+    filter_chain = filter_tpl | llm
+    
+    inputs = {
+        "service_name": serviceName,
+        "searchResults": json.dumps(searchResults)
+    }
+    
+    output = filter_chain.invoke(inputs)
+
+    # Get the website content
+    print(f"Identified the best URL for {serviceName}: {output.content}")
+    url = output.content
+    armSpec = scrape_website(url=url, objective="summarize to be used to generate an ARM template. So keep all JSON properties, descriptions and examples.", summarizeText=False)
+
+
+    # Selec the URL from the list of search results
+    tpl = """
+        Filter out the ARM specification page and ONLY return the 'Resource format' part, 
+        which contains the JSON schema for the ARM template.
+
+        ## begin of documentation
+        {documentation}
+        ## end of documentation
+    """
+    filter_tpl = PromptTemplate(template=tpl, input_variables=["documentation"])   
+    filter_chain = filter_tpl | llm
+    
+    inputs = {
+        "documentation": armSpec,
+    }
+
+    resource_format = filter_chain.invoke(inputs)
+
+    result = {
+        "serviceName": serviceName,
+        "url": url,
+        "resource_format": resource_format.content,
+        "raw_arm_spec": armSpec
+    }
+
+    return result
